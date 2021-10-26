db320c397c0d028a3fbaee1164f0907d72dcf6b9
ilyass <ilyassrhejjou@yahoo.com>
2021-10-26 02:15:21 +0200

    homepage edited

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -7,7 +7,7 @@ export default function Topbar({ menuOpen, setMenuOpen }) {
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">
-            RHEJJOU Ilyass
+            RHEJJOU.
           </a>
           <div className="itemContainer">
             <Person className="icon" />
